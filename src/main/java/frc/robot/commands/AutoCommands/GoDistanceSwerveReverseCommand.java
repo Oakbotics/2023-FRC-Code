a4029385d5f01fbd1d1304b32b20dbe3d89ee2d3
MilindMaheshwari <maheshwarim297@gmail.com>
2023-03-19 11:50:20 -0400

    Added Distance reverse command , adpated mid cube to it

---
+++ src/main/java/frc/robot/commands/AutoCommands/GoDistanceSwerveReverseCommand.java
@@ -0,0 +1,87 @@
+package frc.robot.commands.AutoCommands;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+
+
+public class GoDistanceSwerveReverseCommand  {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    
+    private final DriveSubsystem m_driveSubsystem; 
+    // private final LimelightSubsystem m_limelightSubsystem;
+    private final TrajectoryConfig config = new TrajectoryConfig(
+        AutoConstants.kMaxSpeedMetresPerSecond,
+        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(DriveConstants.kDriveKinematics)
+        .setReversed(true);
+
+    private Trajectory exampleTrajectory; 
+        
+    ProfiledPIDController thetaController = new ProfiledPIDController(
+        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+
+    SwerveControllerCommand swerveControllerCommand;
+    
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param driveSubsytem The subsystem used by this command.
+   */
+   public GoDistanceSwerveReverseCommand(DriveSubsystem driveSubsytem, LimelightSubsystem limelightSubsystem, Pose2d startingPose) {
+    m_driveSubsystem = driveSubsytem;
+    // m_limelightSubsystem = limelightSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    exampleTrajectory= TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        //m_limelightSubsystem.getRobotPose(),
+        startingPose,
+        // Pass through these two interior waypoints, making ans  's' curve path
+        List.of(),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(0,0, new Rotation2d(0)),
+
+        config);
+
+        SmartDashboard.putNumber("BotPoseX", limelightSubsystem.getRobotPose().getX());
+        SmartDashboard.putNumber("BotPoseY", limelightSubsystem.getRobotPose().getY());
+        SmartDashboard.putNumber("DestinationX", startingPose.getX());
+        SmartDashboard.putNumber("DestinationY", startingPose.getY());
+        SmartDashboard.putString("Trajectory", exampleTrajectory.toString());
+        SmartDashboard.putString("Theta Controller Setpoint", thetaController.getSetpoint().toString());
+    swerveControllerCommand = new SwerveControllerCommand(
+        exampleTrajectory,
+        m_driveSubsystem::getPose, // Functional interface to feed supplier
+        DriveConstants.kDriveKinematics,
+
+        // Position controllers
+        new PIDController(AutoConstants.kPXController, 0, 0),
+        new PIDController(AutoConstants.kPYController, 0, 0),
+        thetaController,
+        m_driveSubsystem::setModuleStates,
+        m_driveSubsystem);
+
+
+    }
+
+    public SwerveControllerCommand getAutonomousCommand(){
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        m_driveSubsystem.resetOdometry(exampleTrajectory.getInitialPose());
+        return swerveControllerCommand;
+    }
+
+}
--- src/main/java/frc/robot/commands/AutoCommands/commandGroups/AutoSwerveCommandMidCube.java
+++ src/main/java/frc/robot/commands/AutoCommands/commandGroups/AutoSwerveCommandMidCube.java
@@ -12,6 +12,7 @@ import frc.robot.commands.CandleAnimateCommand;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.OuttakeCommand;
 import frc.robot.commands.ShoulderDropCommand;
+import frc.robot.commands.AutoCommands.GoDistanceSwerveReverseCommand;
 import frc.robot.commands.AutoCommands.GoToPositionSwerveReverseCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.CandleSubsystem;
@@ -53,14 +54,14 @@ public class AutoSwerveCommandMidCube extends SequentialCommandGroup {
         new InstantCommand(()-> m_driveSubsystem.zeroHeading(), m_driveSubsystem),
         new DriveCommand(m_driveSubsystem, 0, 0, 0).withTimeout(1),
         new ArmCommandLow(m_armSubsystem),
-        new GoToPositionSwerveReverseCommand(m_driveSubsystem, m_limelightSubsystem, new Pose2d(new Translation2d(2.20,0), Rotation2d.fromDegrees(0))).getAutonomousCommand(),
+        new GoDistanceSwerveReverseCommand(m_driveSubsystem, m_limelightSubsystem, new Pose2d(new Translation2d(0.15,0), Rotation2d.fromDegrees(0))).getAutonomousCommand(),
         new ArmCommandMidCube(m_armSubsystem),
         // new ShoulderDropCommand(armSubsystem, intakeSubsystem),
         new InstantCommand(()-> m_intakeSubsystem.setIdleModeBrake(false)),
         new OuttakeCommand(m_intakeSubsystem).repeatedly().withTimeout(1),
         new ArmCommandLow(m_armSubsystem),
         new InstantCommand(()-> m_intakeSubsystem.setIdleModeBrake(true)),
-        new GoToPositionSwerveReverseCommand(m_driveSubsystem, m_limelightSubsystem, new Pose2d(new Translation2d(0,0), Rotation2d.fromDegrees(0))).getAutonomousCommand(),
+        new GoDistanceSwerveReverseCommand(m_driveSubsystem, m_limelightSubsystem, new Pose2d(new Translation2d(2.40,0), Rotation2d.fromDegrees(0))).getAutonomousCommand(),
         new InstantCommand(()-> m_driveSubsystem.zeroHeading(180), m_driveSubsystem),
         new RunCommand(()-> m_driveSubsystem.setX(), m_driveSubsystem)
        

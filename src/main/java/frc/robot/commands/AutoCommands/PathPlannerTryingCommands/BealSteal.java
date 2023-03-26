f628a58ee195cecea53e0f7c2b36b3a0bc3a751b
shidai287 <shidai287@gotvdsb.ca>
2023-03-26 14:53:45 -0400

    Basic high cone balance pathplanner

--- src/main/java/frc/robot/commands/AutoCommands/PathPlannerTryingCommands/BealSteal.java
+++ src/main/java/frc/robot/commands/AutoCommands/PathPlannerTryingCommands/BealSteal.java
@@ -3,6 +3,8 @@ package frc.robot.commands.AutoCommands.PathPlannerTryingCommands;
 
 import com.pathplanner.lib.PathConstraints;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
@@ -27,12 +29,14 @@ public class BealSteal extends SequentialCommandGroup {
         // Play command sequence.
         // Added Instant command to reset the speed of the Swerve to 100% to ensure that it is not in slowmode and can successfully auto level.
         addCommands( 
-            // new ArmCommandLow(armSubsystem),
+            new InstantCommand(()-> driveSubsystem.zeroHeading(), driveSubsystem),
+            new ArmCommandLow(armSubsystem),
             new AutoPath("Back30cm", velocity, driveSubsystem),
-            // new ArmCommandHigh(armSubsystem),
-            new AutoPath("Front30cm", velocity, driveSubsystem)
-            // new OuttakeCommand(intakeSubsystem).repeatedly().withTimeout(1)
-            // new AutoPath("ToChargeStation", velocity, driveSubsystem)
+            new ArmCommandHigh(armSubsystem),
+            new AutoPath("Front30cm", velocity, driveSubsystem),
+            new OuttakeCommand(intakeSubsystem).repeatedly().withTimeout(1),
+            new ArmCommandLow(armSubsystem),
+            new AutoPath("ToChargeStation", velocity, driveSubsystem)
             
                 
         );
